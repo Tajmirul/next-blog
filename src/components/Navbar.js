96b7c4982a276fb1dfd1c5ca3f6e86354c7ae132
Tajmriul <tasmirolislam@gmail.com>
2021-01-22 00:16:23 +0600

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,12 @@ import { Container } from 'reactstrap';
 import logo from '../images/logo-dark.svg';
 import { BsArrowUp } from 'react-icons/bs';
 import { MdMenu } from 'react-icons/md';
+import { GrFormClose } from 'react-icons/gr'
 
 
 const Navbar = () => {
     const [stickyNav, setStickyNav] = useState(false);
+    const [openMobileMenu, setOpenMobileMenu] = useState(false);
 
     window.addEventListener('scroll', () => {
         if (window.pageYOffset > 0) {
@@ -24,26 +26,33 @@ const Navbar = () => {
             <header>
                 <nav className={stickyNav ? 'navbar py-3' : 'navbar py-25'}>
                     <Container>
-                        <div className='logo-area'>
+                        <div className='logo-area d-block w-100 d-md-inline w-md-auto'>
                             <Link to='/'><img src={logo} alt='Next Blog' /> </Link>
-                            <div className='hamburger'>
-                                <MdMenu />
-                            </div>
+                            <button className='hamburger d-md-none' onClick={() => setOpenMobileMenu(!openMobileMenu)}>
+                                {openMobileMenu ? <GrFormClose /> : <MdMenu />}
+                            </button>
                         </div>
 
-                            <div className='desktop-nav d-none d-md-block'>
-                                <ul>
-                                    <li><Link to='/'>HOME</Link> </li>
-                                    <li><Link to='/blogs'>BLOGS</Link> </li>
-                                    <li><Link to='/about'>404</Link> </li>
-                                    <li><Link to='/search/web-design'>WEB DESIGN</Link> </li>
-                                    <li><Link to='/contact'>CONTACT</Link> </li>
-                                </ul>
-                            </div>
+                        <div className='desktop-nav d-none d-md-block'>
+                            <ul>
+                                <li><Link to='/'>HOME</Link> </li>
+                                <li><Link to='/blogs'>BLOGS</Link> </li>
+                                <li><Link to='/about'>404</Link> </li>
+                                <li><Link to='/search/web-design'>WEB DESIGN</Link> </li>
+                                <li><Link to='/contact'>CONTACT</Link> </li>
+                            </ul>
+                        </div>
 
-                            <div className='mobile-nav d-md-none'>
-
-                            </div>
+                        <div className={openMobileMenu ? 'mobile-nav d-md-none active' : 'mobile-nav d-md-none'}>
+                            <ul>
+                                <li><Link to='/'>HOME</Link> </li>
+                                <li><Link to='/blogs'>BLOGS</Link> </li>
+                                <li><Link to='/about'>404</Link> </li>
+                                <li><Link to='/search/web-design'>WEB DESIGN</Link> </li>
+                                <li><Link to='/contact'>CONTACT</Link> </li>
+                            </ul>
+                        </div>
+                        {openMobileMenu && <div className='overlay' onClick={() => setOpenMobileMenu(!openMobileMenu)}></div>}
                     </Container>
                 </nav>
             </header>
