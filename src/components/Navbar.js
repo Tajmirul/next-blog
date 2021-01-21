17fb8e7b5232ec9bd7b9577a4bb2baf57c02a5e9
Tajmriul <tasmirolislam@gmail.com>
2021-01-21 19:10:56 +0600

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { Container } from 'reactstrap';
 import logo from '../images/logo-dark.svg';
 import { BsArrowUp } from 'react-icons/bs';
+import { MdMenu } from 'react-icons/md';
 
 
 const Navbar = () => {
@@ -18,28 +19,31 @@ const Navbar = () => {
 
     return (
         <>
-            <button className={stickyNav ? 'back-to-top' : 'back-to-top hidden'} onClick={() => window.scroll(0,0)}><BsArrowUp /></button>
+            <button className={stickyNav ? 'back-to-top' : 'back-to-top hidden'} onClick={() => window.scroll(0, 0)}><BsArrowUp /></button>
 
             <header>
                 <nav className={stickyNav ? 'navbar py-3' : 'navbar py-25'}>
                     <Container>
                         <div className='logo-area'>
                             <Link to='/'><img src={logo} alt='Next Blog' /> </Link>
+                            <div className='hamburger'>
+                                <MdMenu />
+                            </div>
                         </div>
 
-                        <div className='desktop-nav d-none d-md-block'>
-                            <ul>
-                                <li><Link to='/'>HOME</Link> </li>
-                                <li><Link to='/blogs'>BLOGS</Link> </li>
-                                <li><Link to='/about'>404</Link> </li>
-                                <li><Link to='/search/web-design'>WEB DESIGN</Link> </li>
-                                <li><Link to='/contact'>CONTACT</Link> </li>
-                            </ul>
-                        </div>
+                            <div className='desktop-nav d-none d-md-block'>
+                                <ul>
+                                    <li><Link to='/'>HOME</Link> </li>
+                                    <li><Link to='/blogs'>BLOGS</Link> </li>
+                                    <li><Link to='/about'>404</Link> </li>
+                                    <li><Link to='/search/web-design'>WEB DESIGN</Link> </li>
+                                    <li><Link to='/contact'>CONTACT</Link> </li>
+                                </ul>
+                            </div>
 
-                        <div className='mobile-nav d-md-none'>
+                            <div className='mobile-nav d-md-none'>
 
-                        </div>
+                            </div>
                     </Container>
                 </nav>
             </header>
